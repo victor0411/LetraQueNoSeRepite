80726905b137c7e1d807c41ea68f92fa7352f6e2
victor0411 <54162701+victor0411@users.noreply.github.com>
2019-08-25 23:16:18 -0500

    Documentacion Main

--- src/main/java/com/mycompany/letraquenoserepite/Main.java
+++ src/main/java/com/mycompany/letraquenoserepite/Main.java
@@ -1,13 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase Main por donde el programa Inicia
+ * 
+ * 
  */
 package com.mycompany.letraquenoserepite;
 
 /**
  *
- * @author SuperUs
+ * @author Victor Preciado
+ * @author Edward Ramos
  */
 public class Main {
 
