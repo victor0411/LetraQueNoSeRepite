14e8190d64f1c1b72e732019455816680948c448
victor0411 <54162701+victor0411@users.noreply.github.com>
2019-08-25 23:07:00 -0500

    Declaracion de Variables

--- src/main/java/com/mycompany/letraquenoserepite/Principal.java
+++ src/main/java/com/mycompany/letraquenoserepite/Principal.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package com.mycompany.letraquenoserepite;
-
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
 /**
  *
  * @author SuperUs
@@ -14,8 +16,36 @@ public class Principal {
     
     public void inicio(){
     
+        Scanner sc = new Scanner(System.in);
+        private String cadena;
+        private char[] caracter;
+        Vector<Character> vector = new Vector<Character>();
+        
+         public char[] getCaracter() {
+        return caracter;
+    }
+
+    public void setCaracter(char[] caracter) {
+        this.caracter = caracter;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+   
     
+    public String setCadena() {
+        return cadena;
+    }
     
+    public Scanner getSc() {
+        return sc;
+    }
+
+   
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
     }
     
 }
